506f47d6126bc40df4004905e415da666a0c3f92
Riya Mary Jude <97887337+Riyajude@users.noreply.github.com>
2024-10-04 12:38:54 +0530

    Update minimisedfa.c

--- minimisedfa.c
+++ minimisedfa.c
@@ -5,44 +5,35 @@
 #define MAX_STATES 100
 #define MAX_ALPHABETS 26
 
-typedef struct {
-    int numStates;
-    int numAlphabets;
-    int transitions[MAX_STATES][MAX_ALPHABETS];
-    bool isFinal[MAX_STATES];
-    int startState;
-} DFA;
-
+int dfa[MAX_STATES][MAX_ALPHABETS];
+bool finalStates[MAX_STATES];
 bool distinguishable[MAX_STATES][MAX_STATES];
-int equivalentClass[MAX_STATES];
-bool marked[MAX_STATES][MAX_STATES];
+int newStates[MAX_STATES];
+int numStates, numAlphabets, startState, numFinalStates;
 
-void initializeTable(DFA *dfa) {
-    // Initialize distinguishable table with false
-    memset(distinguishable, 0, sizeof(distinguishable));
-
-    // Mark distinguishable pairs between final and non-final states
-    for (int i = 0; i < dfa->numStates; i++) {
-        for (int j = 0; j < dfa->numStates; j++) {
-            if (dfa->isFinal[i] != dfa->isFinal[j]) {
+void initializeTable() {
+    // Initialize the distinguishable table
+    for (int i = 0; i < numStates; i++) {
+        for (int j = 0; j < numStates; j++) {
+            if (i != j && (finalStates[i] != finalStates[j])) {
                 distinguishable[i][j] = true;
+            } else {
+                distinguishable[i][j] = false;
             }
         }
     }
 }
 
-void markDistinguishable(DFA *dfa) {
-    bool changed = true;
-    while (changed) {
+void markDistinguishable() {
+    bool changed;
+    do {
         changed = false;
-
-        for (int i = 0; i < dfa->numStates; i++) {
+        for (int i = 0; i < numStates; i++) {
             for (int j = 0; j < i; j++) {
                 if (!distinguishable[i][j]) {
-                    for (int symbol = 0; symbol < dfa->numAlphabets; symbol++) {
-                        int iNext = dfa->transitions[i][symbol];
-                        int jNext = dfa->transitions[j][symbol];
-
+                    for (int symbol = 0; symbol < numAlphabets; symbol++) {
+                        int iNext = dfa[i][symbol];
+                        int jNext = dfa[j][symbol];
                         if (iNext != -1 && jNext != -1 && distinguishable[iNext][jNext]) {
                             distinguishable[i][j] = true;
                             changed = true;
@@ -52,121 +43,85 @@ void markDistinguishable(DFA *dfa) {
                 }
             }
         }
-    }
+    } while (changed);
 }
 
-void computeEquivalentClasses(DFA *dfa) {
-    for (int i = 0; i < dfa->numStates; i++) {
-        equivalentClass[i] = i;
-    }
+void minimizeDFA() {
+    initializeTable();
+    markDistinguishable();
 
-    int classCount = dfa->numStates;
-
-    for (int i = 0; i < dfa->numStates; i++) {
-        for (int j = 0; j < i; j++) {
-            if (!distinguishable[i][j]) {
-                equivalentClass[j] = equivalentClass[i];
-            }
-        }
-    }
-}
-
-void minimizeDFA(DFA *dfa) {
-    initializeTable(dfa);
-    markDistinguishable(dfa);
-    computeEquivalentClasses(dfa);
-
-    DFA minimizedDFA;
-    minimizedDFA.numAlphabets = dfa->numAlphabets;
-
-    // Create minimized DFA by merging equivalent states
-    int stateMapping[MAX_STATES] = {0};
     int newNumStates = 0;
 
-    for (int i = 0; i < dfa->numStates; i++) {
-        if (equivalentClass[i] == i) {
-            stateMapping[i] = newNumStates++;
-        }
-        else {
-            stateMapping[i] = stateMapping[equivalentClass[i]];
-        }
+    // Initialize newStates array to -1
+    for (int i = 0; i < numStates; i++) {
+        newStates[i] = -1;
     }
 
-    minimizedDFA.numStates = newNumStates;
-    minimizedDFA.startState = stateMapping[dfa->startState];
-
-    for (int i = 0; i < dfa->numStates; i++) {
-        int newState = stateMapping[i];
-        minimizedDFA.isFinal[newState] = dfa->isFinal[i];
-    }
-
-    for (int i = 0; i < dfa->numStates; i++) {
-        for (int symbol = 0; symbol < dfa->numAlphabets; symbol++) {
-            int nextState = dfa->transitions[i][symbol];
-            if (nextState != -1) {
-                minimizedDFA.transitions[stateMapping[i]][symbol] = stateMapping[nextState];
+    // Assign new state numbers based on equivalence classes
+    for (int i = 0; i < numStates; i++) {
+        if (newStates[i] == -1) {
+            for (int j = i; j < numStates; j++) {
+                if (!distinguishable[i][j]) {
+                    newStates[j] = newNumStates;
+                }
             }
-            else {
-                minimizedDFA.transitions[stateMapping[i]][symbol] = -1;
-            }
+            newNumStates++;
         }
     }
 
-    printf("\nMinimized DFA:\n");
-    printf("Number of states: %d\n", minimizedDFA.numStates);
-    printf("Start state: q%d\n", minimizedDFA.startState + 1);
+    // Print minimized DFA
+    printf("Minimized DFA:\n");
+    printf("Number of states: %d\n", newNumStates);
+    printf("Start state: q%d\n", newStates[startState] + 1);
     
     printf("Final states: ");
-    for (int i = 0; i < minimizedDFA.numStates; i++) {
-        if (minimizedDFA.isFinal[i]) {
-            printf("q%d ", i + 1);
+    for (int i = 0; i < numStates; i++) {
+        if (finalStates[i]) {
+            printf("q%d ", newStates[i] + 1);
         }
     }
     printf("\nTransitions:\n");
-    
-    for (int i = 0; i < minimizedDFA.numStates; i++) {
-        for (int symbol = 0; symbol < minimizedDFA.numAlphabets; symbol++) {
-            if (minimizedDFA.transitions[i][symbol] != -1) {
-                printf("q%d -- %c --> q%d\n", i + 1, 'a' + symbol, minimizedDFA.transitions[i][symbol] + 1);
+
+    for (int i = 0; i < numStates; i++) {
+        for (int symbol = 0; symbol < numAlphabets; symbol++) {
+            if (dfa[i][symbol] != -1) {
+                printf("q%d -- %c --> q%d\n", newStates[i] + 1, 'a' + symbol, newStates[dfa[i][symbol]] + 1);
             }
         }
     }
 }
 
 int main() {
-    DFA dfa;
     int numTransitions;
-
+    
     // Input number of alphabets
     printf("Enter the number of alphabets: ");
-    scanf("%d", &dfa.numAlphabets);
+    scanf("%d", &numAlphabets);
 
     // Input number of states
     printf("Enter the number of states: ");
-    scanf("%d", &dfa.numStates);
+    scanf("%d", &numStates);
 
     // Input start state
     printf("Enter the start state: ");
-    scanf("%d", &dfa.startState);
-    dfa.startState--; // Adjust to 0-based index
+    scanf("%d", &startState);
+    startState--; // Adjust to 0-based index
 
     // Input number of final states
     printf("Enter the number of final states: ");
-    int numFinalStates;
     scanf("%d", &numFinalStates);
 
     printf("Enter the final states: ");
-    memset(dfa.isFinal, false, sizeof(dfa.isFinal));
     for (int i = 0; i < numFinalStates; i++) {
         int state;
         scanf("%d", &state);
-        dfa.isFinal[state - 1] = true; // Adjust to 0-based index
+        finalStates[state - 1] = true; // Adjust to 0-based index
     }
 
-    // Initialize transitions to -1 (indicating no transition)
-    for (int i = 0; i < MAX_STATES; i++) {
-        for (int j = 0; j < MAX_ALPHABETS; j++) {
-            dfa.transitions[i][j] = -1;
+    // Initialize transitions to -1 (no transition)
+    for (int i = 0; i < numStates; i++) {
+        for (int j = 0; j < numAlphabets; j++) {
+            dfa[i][j] = -1;
         }
     }
 
@@ -179,11 +134,11 @@ int main() {
         int fromState, toState;
         char symbol;
         scanf("%d %c %d", &fromState, &symbol, &toState);
-        dfa.transitions[fromState - 1][symbol - 'a'] = toState - 1; // Adjust to 0-based index
+        dfa[fromState - 1][symbol - 'a'] = toState - 1; // Adjust to 0-based index
     }
 
     // Minimize DFA
-    minimizeDFA(&dfa);
+    minimizeDFA();
 
     return 0;
 }
