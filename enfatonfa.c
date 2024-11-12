7348ca2c4c53447d29cea0c4a05210d26ec57e8a
Riya Mary Jude <97887337+Riyajude@users.noreply.github.com>
2024-11-12 21:39:03 +0530

    Update enfatonfa.c

--- enfatonfa.c
+++ enfatonfa.c
@@ -1,167 +1,124 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-#define MAX_STATES 100
-#define MAX_ALPHABETS 26
-
-typedef struct {
-    int numStates;
-    int numAlphabets;
-    bool transitions[MAX_STATES][MAX_ALPHABETS][MAX_STATES];
-    bool epsilonTransitions[MAX_STATES][MAX_STATES];
-    bool isFinal[MAX_STATES];
-    int startState;
-} NFA;
-
-// Function to calculate epsilon closure for a state
-void epsilonClosure(int state, bool closure[], NFA *nfa) {
-    closure[state] = true;
-    for (int i = 0; i < nfa->numStates; i++) {
-        if (nfa->epsilonTransitions[state][i] && !closure[i]) {
-            epsilonClosure(i, closure, nfa);
-        }
-    }
+#include<stdio.h>
+typedef struct Node{
+	int transition[5];
+	}Node;
+Node node[10];	
+// e_closure() find and store e-closures to e_arr
+int e_arr[10];
+// intermediate e-closure calculated are stored in e_sub_arr
+int e_sub_arr[10];
+// contains the alphabets a,b,c......
+char alphabets[10];
+// map characters to an index value a-1, b-2 .....
+int map(char a){
+	if( a =='e' )
+		return 0;	
+	int i;
+	i=a-96;
+	return i;
+}
+// find e-closure of node e and store the result(indexes) in res[]
+int e_closure(int e, int res[]){
+	int count=0, i;
+	res[count++] = e; // e closure contain itself
+	while(node[e].transition[ map('e') ] !=-1 ){// check if there is an e transition
+			i = node[e].transition[ map('e') ]; // return index of next node
+			res[count++] = i;
+			e = i;// set new node as current node 
+	}
+	return count;
+}
+int get_transition(int e,char a){
+	if(node[e].transition[ map(a) ] !=-1 ){// check if there is the req transition
+			int i = node[e].transition[ map(a) ]; // return index of next node
+			return i;
+	}
+	return -1;
+}
+void perform(int n,int alpha){
+	int i,c,j,r[50],b,p,k,w,copy[50],q;
+	for(i=0;i<n;i++){
+		c=e_closure(i,e_arr);// finding e-closure
+		// finding transition for each alphabet
+		for(k=0;k<alpha;k++){	
+			int cnt=0;
+			for(j=0;j<c;j++){
+				int flag;
+				flag=get_transition(e_arr[j],alphabets[k]);
+				if(flag!=-1){// return -1 if no transition
+					r[cnt++]=flag;// cnt count no of items	
+				}
+			} // finding transition of it for each alphabet
+			int count=0;// count no of states after taking 2nd e-closure
+			for( w=0;w<cnt;w++){
+				b = e_closure(r[w],e_sub_arr);
+				for(q=0;q<b;q++){
+					copy[count++] = e_sub_arr[q];		
+				}
+			}
+			printf("nState-> %d for alphabet %c >> { ",i,alphabets[k]);
+			int print;
+			for(p=0;p<count;p++){
+				print=1;
+				for(int r=p-1;r>0;r--){
+					if(copy[p] == copy[r])
+						print=0;
+				}
+				if( print==1)		
+					printf(" %d",copy[p]);
+			}
+			printf(" } \n");
+		}
+	}
+}	
+int main(){
+	/*variables in main()
+	 * 	INTEGERS
+	 * n - no of nodes
+	 * t - no of transitions
+	 * a - no of alphabets 
+	 *  -1 # indicates No transition for that input alphabet
+	 * */
+	int n,t,a,state1,state2,i,j;
+	char alpha;
+	printf("\n Enter the no of nodes :");
+	scanf("%d",&n);
+	printf("\n Enter the no of alphabets: ");
+	scanf("%d",&a);
+	for(i=0;i<a;i++)
+		scanf(" %c",&alphabets[i]);
+		
+	for(i=0;i<n;i++){
+		for(j=0;j<a+1;j++)// for initializing with no transition, a+1 for including epsilon
+			node[i].transition[j] = -1;
+			
+	}
+	printf("\nEnter the no of transitions: ");
+	scanf("%d", &t);
+	printf("\nEnter the state table:>>nSTATE ALPHABET STATE\n");
+	for(i=0;i<t;i++){
+		scanf("%d	%c	%d",&state1,&alpha,&state2);
+		node[state1].transition[ map(alpha) ] = state2;
+	}
+	perform(n,a);
 }
 
-// Function to convert epsilon-NFA to NFA
-void convertToNFA(NFA *nfa) {
-    bool closure[MAX_STATES][MAX_STATES] = {false};
+/*
+enfa
+0 a 0
+0 e 1
+1 b 1
+1 e 2
+2 c 2
 
-    // Calculate epsilon closure for each state
-    for (int i = 0; i < nfa->numStates; i++) {
-        epsilonClosure(i, closure[i], nfa);
-    }
-
-    bool newTransitions[MAX_STATES][MAX_ALPHABETS][MAX_STATES] = {false};
-
-    // Adjust transitions using epsilon closure
-    for (int state = 0; state < nfa->numStates; state++) {
-        for (int symbol = 0; symbol < nfa->numAlphabets - 1; symbol++) {
-            for (int epsState = 0; epsState < nfa->numStates; epsState++) {
-                if (closure[state][epsState]) {
-                    for (int dest = 0; dest < nfa->numStates; dest++) {
-                        if (nfa->transitions[epsState][symbol][dest]) {
-                            for (int epsDest = 0; epsDest < nfa->numStates; epsDest++) {
-                                if (closure[dest][epsDest]) {
-                                    newTransitions[state][symbol][epsDest] = true;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // Copy the new transitions
-    for (int i = 0; i < nfa->numStates; i++) {
-        for (int symbol = 0; symbol < nfa->numAlphabets - 1; symbol++) {
-            for (int j = 0; j < nfa->numStates; j++) {
-                nfa->transitions[i][symbol][j] = newTransitions[i][symbol][j];
-            }
-        }
-    }
-
-    // Adjust final states
-    for (int state = 0; state < nfa->numStates; state++) {
-        for (int epsState = 0; epsState < nfa->numStates; epsState++) {
-            if (closure[state][epsState] && nfa->isFinal[epsState]) {
-                nfa->isFinal[state] = true;
-            }
-        }
-    }
-}
-
-// Helper function to print NFA details
-void printNFA(NFA *nfa) {
-    printf("Start state: {%d}\n", nfa->startState + 1);
-    printf("Alphabets: ");
-    for (int i = 0; i < nfa->numAlphabets - 1; i++) {
-        printf("%c ", 'a' + i);
-    }
-    printf("\nStates: ");
-    for (int i = 0; i < nfa->numStates; i++) {
-        printf("q%d ", i + 1);
-    }
-    printf("\nTransitions:\n");
-
-    for (int state = 0; state < nfa->numStates; state++) {
-        for (int symbol = 0; symbol < nfa->numAlphabets - 1; symbol++) {
-            printf("q%d %c: {", state + 1, 'a' + symbol);
-            for (int dest = 0; dest < nfa->numStates; dest++) {
-                if (nfa->transitions[state][symbol][dest]) {
-                    printf(" q%d", dest + 1);
-                }
-            }
-            printf(" }\n");
-        }
-    }
-
-    printf("Final states: ");
-    for (int i = 0; i < nfa->numStates; i++) {
-        if (nfa->isFinal[i]) {
-            printf("q%d ", i + 1);
-        }
-    }
-    printf("\n");
-}
-
-int main() {
-    NFA nfa;
-    int numFinalStates, numTransitions;
-    char alphabets[MAX_ALPHABETS];
-
-    // Input number of alphabets
-    printf("Enter the number of alphabets: ");
-    scanf("%d", &nfa.numAlphabets);
-
-    printf("Enter the alphabets (e for epsilon): ");
-    for (int i = 0; i < nfa.numAlphabets; i++) {
-        scanf(" %c", &alphabets[i]);
-    }
-
-    // Input number of states
-    printf("Enter the number of states: ");
-    scanf("%d", &nfa.numStates);
-
-    // Input start state
-    printf("Enter the start state: ");
-    scanf("%d", &nfa.startState);
-    nfa.startState--; // Adjust to 0-based index
-
-    // Input number of final states
-    printf("Enter the number of final states: ");
-    scanf("%d", &numFinalStates);
-
-    printf("Enter the final states: ");
-    for (int i = 0; i < numFinalStates; i++) {
-        int state;
-        scanf("%d", &state);
-        nfa.isFinal[state - 1] = true; // Adjust to 0-based index
-    }
-
-    // Input transitions
-    printf("Enter the number of transitions: ");
-    scanf("%d", &numTransitions);
-
-    for (int i = 0; i < numTransitions; i++) {
-        int fromState, toState;
-        char symbol;
-        printf("Enter transition (fromState symbol toState): ");
-        scanf("%d %c %d", &fromState, &symbol, &toState);
-
-        if (symbol == 'e') {
-            nfa.epsilonTransitions[fromState - 1][toState - 1] = true; // Adjust to 0-based index
-        } else {
-            nfa.transitions[fromState - 1][symbol - 'a'][toState - 1] = true; // Adjust to 0-based index
-        }
-    }
-
-    // Convert epsilon-NFA to NFA
-    convertToNFA(&nfa);
-
-    // Output the converted NFA
-    printNFA(&nfa);
-
-    return 0;
-}
+output 
+0 a -> 0,1,2
+0 b -> 1,2
+0 c -> 2
+1 a -> -
+1 b ->1,2
+1 c -> 2
+2 a -> -
+2 b -> -
+2 c -> 2
+*/
